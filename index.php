0eadf97ed939aaa3c87abfd115c1f9add70b57e4
Júlia Souza <64169914+julia-souza@users.noreply.github.com>
2022-09-30 20:24:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -28,18 +28,27 @@ for($i = 0; $i < $qtdMatricula; $i++){
     $matriculaAluno = ($matricula[$i]["matricula"]);
 
     echo $cracha = '
-        <div class="cracha">
+        <div class="cracha" 
+        style="margin: auto;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+        flex-wrap: wrap;
+        border: 1px solid;
+        width: max-content;">
             <div class="crachaH">
-                <img src="./img/download-removebg-preview.png">
-                <img src="./img/jacitec-removebg-preview.png">
+                <img src="./img/download-removebg-preview.png" style="width:450px">
+                <img src="./img/jacitec-removebg-preview.png" style="width:450px">
             </div>
-            <div class="crachaBody">
+            <div class="crachaBody" style="font-size:3rem; font-family:Roboto">
                 <h1> JACITEC 2022 </h1>
+                <p> ' . $nomeAluno . ' </p>
                 <p> ' . $matriculaAluno . '</p>
                 <p> ' . $generator->getBarcode(
-                    $matriculaAluno,
-                    $generator::TYPE_CODE_128
-                ) . '</p>
+                        $matriculaAluno,
+                        $generator::TYPE_CODE_128
+                    ) . '</p>
             </div>
         </div>
     ';
