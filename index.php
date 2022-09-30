504b90467ef6168b98756563c270051558deae57
Júlia Souza <juliadsouzaborges@gmail.com>
2022-09-30 19:57:27 -0300

    consumindo dados de CSV

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
-<head>
-    <link rel="stylesheet" href="cracha.css">
-</head>
-<?php
-
+<?php 
 include "vendor/autoload.php";
 
-$matricula = "20191si032"; //deixar dinâmico
+// $matricula = "20191si032"; //deixar dinâmico   
 
+//gerar código de barras
 $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
 
-$cracha = '
+//ler csv com a matrícula
+$path = "data.csv";
+
+$handle = fopen($path, "r");
+$header = fgetcsv($handle, 10000000, ",");
+
+$qtdMatricula = 0;
+
+while ($row = fgetcsv($handle, 1000, ",")) {
+    $matricula[] = array_combine($header, $row);
+    $qtdMatricula ++;
+}
+// print_r($matricula);
+fclose($handle);
+// echo $qtdMatricula;
+
+//selecionar apenas o número de matrícula
+$qtdMatricula = 1;
+for($i = 0; $i < $qtdMatricula; $i++){
+    $matriculaAluno = ($matricula[$i]["matricula"]);
+
+    echo $cracha = '
         <div class="cracha">
             <div class="crachaH">
                 <img src="./img/download-removebg-preview.png">
@@ -17,14 +35,13 @@ $cracha = '
             </div>
             <div class="crachaBody">
                 <h1> JACITEC 2022 </h1>
-                <p> ' . $matricula . '</p>
+                <p> ' . $matriculaAluno . '</p>
                 <p> ' . $generator->getBarcode(
-                    $matricula,
+                    $matriculaAluno,
                     $generator::TYPE_CODE_128
                 ) . '</p>
             </div>
         </div>
     ';
-
-echo $cracha;    
+}
 ?>
